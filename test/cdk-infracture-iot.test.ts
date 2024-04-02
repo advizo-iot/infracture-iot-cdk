0a498e5c7079e7c3b4f89a42e89afe0a56505284
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-04-02 10:28:58 -0500

    Initial commit

---
+++ test/cdk-infracture-iot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkInfractureIot from '../lib/cdk-infracture-iot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-infracture-iot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkInfractureIot.CdkInfractureIotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
