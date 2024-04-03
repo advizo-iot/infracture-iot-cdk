e8c9cfa069a00431ddd1326569a9e8f66a88abe1
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-04-02 21:34:43 -0500

    update endpoints all

--- lib/stack/lambda/main.py
+++ lib/stack/lambda/main.py
@@ -1,7 +1,16 @@
-import boto3
-import json 
+import boto3,json,time
 from datetime import datetime, timedelta
-import time
 
 s3 = boto3.client('s3')
 RESULT_OUTPUT_LOCATION = "s3://energas-datalake/athenadb/queries/"
+
+def lambda_handler(event, context):
+    print("Event: ", event)
+    print("Context: ", context)
+
+    body = json.loads(event["body"])
+    print("Body: ", body)
+    endpoint = dict(body['endpoint'])
+    print("Endpoint: ", endpoint)
+    data_endpoint = dict(endpoint['data'])
+    print("Data Endpoint: ", data_endpoint)
