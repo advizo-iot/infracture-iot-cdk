5fe69b423dc7a23168f538fd1a504f569ff3ef1c
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-04-02 21:00:19 -0500

    add folder resources/s3/main.py

--- resources/s3/main.py
+++ resources/s3/main.py
@@ -2,7 +2,7 @@ import boto3
 
 s3 = boto3.client('s3')
 bucket_name = "advizo-iot-fire-resources"
-folders = ["athenadb/queries","athenadb/input"]
+folders = ["athenadb/input/production_raw_iot_advizo/users"]
 
 def create_folder(folder_name,client,bucket):
     for folder in folder_name:
