d24ce7f0c0a8eb9b99129cb134d2407f627e46a9
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-04-03 13:41:00 -0500

    create athenadb/public/images folder

--- resources/s3/main.py
+++ resources/s3/main.py
@@ -2,7 +2,7 @@ import boto3
 
 s3 = boto3.client('s3')
 bucket_name = "advizo-iot-fire-resources"
-folders = ["athenadb/input/production_raw_iot_advizo/map_factory"]
+folders = ["athenadb/public/images"]
 
 def create_folder(folder_name,client,bucket):
     for folder in folder_name:
