fb718bebd556282e1dff070cd847e28522669dbc
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-04-02 20:44:04 -0500

    create database athena

--- resources/athena/createdb.py
+++ resources/athena/createdb.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3, time
 CLIENT = boto3.client("athena")
 DATABASE_NAME = "production_raw_iot_advizo"
 RESULT_OUTPUT_LOCATION = "s3://advizo-iot-fire-resources/athenadb/queries/"
@@ -19,8 +19,31 @@ def create_database():
     else:
         print(f"Database '{DATABASE_NAME}' already exists. [create_database]")
 
+def has_query_succeeded(execution_id):
+    state = "RUNNING"
+    max_execution = 5
+
+    while max_execution > 0 and state in ["RUNNING", "QUEUED"]:
+        max_execution -= 1
+        response = CLIENT.get_query_execution(QueryExecutionId=execution_id)
+        if (
+            "QueryExecution" in response
+            and "Status" in response["QueryExecution"]
+            and "State" in response["QueryExecution"]["Status"]
+        ):
+            state = response["QueryExecution"]["Status"]["State"]
+            if state == "SUCCEEDED":
+                return True
+
+        time.sleep(5)
+
+    return False
+
 def main():
-    create_database()
+    execution_id = create_database()
+    print(f"Checking query execution for: {execution_id}")
+    query_status = has_query_succeeded(execution_id=execution_id)
+    print(f"Query status: {query_status}")
 
 if __name__ == "__main__":
     main()
